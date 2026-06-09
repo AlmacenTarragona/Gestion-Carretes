fdd4f3142fb3f2b0f0be67b788e843b0362dd314
AlmacenTarragona <almacen.tarragona@gmail.com>
2026-06-09 16:29:41 +0200

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,1 +1,40 @@
+/**
+ * =====================================================
+ * CONFIG.JS
+ * Configuración global
+ * =====================================================
+ */
 
+const CONFIG = {
+
+    API:
+    "https://script.google.com/macros/s/AKfycbz3nDtwEfV7q2UnBsad7CRDHMIrC81FlabTSC64FZRu4BSg1STuBxnj4V54ztm44Xxg/exec"
+
+};
+
+
+/**
+ * Estados permitidos
+ */
+const ESTADOS = [
+
+    "En Almacén",
+
+    "Fuera",
+
+    "Vacía"
+
+];
+
+
+/**
+ * Variables globales
+ */
+
+let datos = [];
+
+let filtroEstado = "";
+
+let textoBusqueda = "";
+
+let carreteSeleccionado = null;
